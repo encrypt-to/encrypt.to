1036f15dcebf25541a08d35f5efb51ef55acf230
Jan Wiegelmann <git@wiegelmann.net>
2013-12-16 18:51:36 +0100

    fixes from to

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,12 +3,12 @@ class MessageMailer < ActionMailer::Base
   
   default from: APP_CONFIG['sender']
   
-  def send_message(from, to, body)
+  def send_message(to, from, body)
     subject = "#{from} has sent you an encrypted mail"
     mail(to: to, reply_to: from, subject: subject, :body => body)
   end
   
-  def thanks_message(from, to)
+  def thanks_message(to, from)
     subject = "Thanks for using Encrypt.to"
     @to = to
     mail(to: from, subject: subject)
