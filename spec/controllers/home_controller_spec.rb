1b3fdf2ac57409674a7f59e30fabec723b28e155
Jan Wiegelmann <git@wiegelmann.net>
2013-12-30 10:56:12 +0100

    start testing

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe HomeController do
+  
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+  
+  describe "GET terms" do
+    it "has a 200 status code" do
+      get :terms
+      expect(response.status).to eq(200)
+    end
+  end
+  
+  describe "GET privacy" do
+    it "has a 200 status code" do
+      get :privacy
+      expect(response.status).to eq(200)
+    end
+  end
+end
