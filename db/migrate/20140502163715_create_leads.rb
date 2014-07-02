602cb192cb6a077b2dbdf2a26640cb5ec5394327
Jan Wiegelmann <git@wiegelmann.net>
2014-07-02 20:10:56 +0200

    remove leads

--- db/migrate/20140502163715_create_leads.rb
+++
@@ -1,8 +0,0 @@
-class CreateLeads < ActiveRecord::Migration
-  def change
-    create_table :leads do |t|
-      t.string :email
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140502163715) do
-
-  create_table "leads", :force => true do |t|
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20131221100555) do
 
   create_table "messages", :force => true do |t|
     t.datetime "created_at", :null => false
